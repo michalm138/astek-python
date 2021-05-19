a1b68083abb418a4e097329d7701fe2c284a44f5
michalm138 <michalmichalak138@gmail.com>
2021-05-19 17:06:44 +0200

    Add database models

--- eMenu_API/models.py
+++ eMenu_API/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    preparation_time = models.PositiveIntegerField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    vegetarian = models.BooleanField()
+
+    class Meta:
+        verbose_name_plural = 'Dishes'
+
+    def __str__(self):
+        return self.name
+
