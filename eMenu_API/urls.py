e579c55db6c3fa79476f89cfa0d687515421a57a
michalm138 <michalmichalak138@gmail.com>
2021-05-22 19:05:14 +0200

    Add swagger - api documentation

--- eMenu_API/urls.py
+++ eMenu_API/urls.py
@@ -1,5 +1,21 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="eMenu API",
+      default_version='v1',
+      description="Test description",
+      contact=openapi.Contact(email="michalmichalak138@gmail.com"),
+   ),
+   public=False,
+   permission_classes=(permissions.IsAuthenticated,),
+)
+
 
 urlpatterns = [
     # Private API endpoints
@@ -10,5 +26,9 @@ urlpatterns = [
     path('private/delete/dish/<int:pk>/', views.DeleteDish.as_view(), name='delete-dish'),
     path('private/update/dish/<int:pk>/', views.UpdateDish.as_view(), name='update-dish'),
     # Public API endpoints
-    path('public/list/menus/', views.MenuList.as_view(), name='list-menus')
+    path('public/list/menus/', views.MenuList.as_view(), name='list-menus'),
+    # Other
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
